9933d840e519573123fff347089e754cef772328
Jake Romer <jkrmr@users.noreply.github.com>
2014-11-17 19:49:39 -0800

    Update for final edition

--- features/codebreaker_starts_game.feature
+++ features/codebreaker_starts_game.feature
@@ -1,4 +1,4 @@
-Feature: code-breaker starts game
+Feature: Code-breaker starts game
   As a code-breaker
   I want to start a game
   So that I can break the code
--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -11,17 +11,17 @@ Then(/^the game should say "(.*?)"$/) do |message|
   expect(@messenger.string.split("\n")).to include(message)
 end
 
-Given(/^the secret code is (. . . .)$/) do |code|
+Given(/^the secret code is "([[:digit:]]{4})"$/) do |code|
   @messenger = StringIO.new
   @game = Mastermind::Game.new(@messenger)
   @game.start(code.split)
 end
 
-When(/^I guess (. . . .)$/) do |code|
+When(/^I guess "([[:digit:]]{4})"$/) do |code|
   @game.guess(code.split)
 end
 
-Then(/^the mark should be (\w{1,4})$/) do |mark|
+Then(/^the mark should be "(.*)"$/) do |mark|
   message_string = @messenger.string.split("\n")
   expect(message_string).to include(mark)
 end
