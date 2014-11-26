41e8ee650be232b81820a3a27ded1339593e748a
Jake Romer <jkrmr@users.noreply.github.com>
2014-11-26 12:37:51 -0800

    Roll back Game definition

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,25 +1,12 @@
 module Codebreaker
   class Game
-    def initialize(messenger)
-      @messenger = messenger
+    def initialize(output)
+      @output = output
     end
 
-    def start(code)
-      @code = code
-      @messenger.puts 'Welcome to Codebreaker!'
-      @messenger.puts 'Enter guess:'
-    end
-
-    def guess(guess)
-      result = guess.each_with_index.with_object([]) do |(peg, idx), chars|
-        if @code[idx] == peg
-          chars << 'b'
-        elsif @code.include? peg
-          chars << 'w'
-        end
-      end
-      @messenger.puts result.sort.join
+    def start
+      @output.puts 'Welcome to Codebreaker!'
+      @output.puts 'Enter guess:'
     end
   end
 end
-
