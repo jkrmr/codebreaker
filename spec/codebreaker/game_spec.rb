15fe3dc06acae97c6f070bb587ca2db8782c0c9f
Jake Romer <jkrmr@users.noreply.github.com>
2014-11-28 12:24:26 -0800

    Update Codebreaker game spec

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,54 +2,24 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
-    context 'starting up' do
-      before :each do
-        @messenger = double('messenger').as_null_object
-        @game = Game.new(@messenger)
+    describe "#start" do
+      it "sends a welcome message" do
+        output = double('output').as_null_object
+        game = Game.new(output)
+
+        expect(output).to receive(:puts).with('Welcome to Codebreaker!')
+
+        game.start
       end
 
-      it 'sends a welcome message (passing the code to start)' do
-        expect(@messenger).to receive(:puts).with('Welcome to Codebreaker!')
-        @game.start %w(r g y c)
-      end
+      it "prompts for the first guess" do
+        output = double('output').as_null_object
+        game = Game.new(output)
 
-      it 'prompts for the first guess' do
-        expect(@messenger).to receive(:puts).with('Enter guess:')
-        @game.start %w(r g y c)
-      end
-    end
+        expect(output).to receive(:puts).with('Enter guess:')
 
-    context 'marking a guess' do
-      context 'with all 4 colors correct and correctly positioned' do
-        it 'marks the guess with bbbb' do
-          messenger = double('messenger').as_null_object
-          game = Game.new(messenger)
-          game.start %w(r g y c)
-          expect(messenger).to receive(:puts).with('bbbb')
-          game.guess %w(r g y c)
-        end
-      end
-
-      context 'with all 4 colors correct and 2 correctly positioned' do
-        it 'marks the guess with bbww' do
-          messenger = double('messenger').as_null_object
-          game = Game.new(messenger)
-          game.start %w(r g y c)
-          expect(messenger).to receive(:puts).with('bbww')
-          game.guess %w(r g c y)
-        end
-      end
-
-      context 'with all 4 colors correct and 1 correctly positioned' do
-        it 'marks the guess with bbww' do
-          messenger = double('messenger').as_null_object
-          game = Game.new(messenger)
-          game.start %w(r g y c)
-          expect(messenger).to receive(:puts).with('bwww')
-          game.guess %w(y r g c)
-        end
+        game.start
       end
     end
   end
 end
-
