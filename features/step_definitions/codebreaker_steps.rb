16cd12f2289fa0badca044d91a5f257bbce8580d
Jake Romer <jkrmr@users.noreply.github.com>
2014-12-13 12:09:17 -0800

    Add pending cucumber steps for submit_guess feature

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -24,3 +24,15 @@ Then /^I should see "([^"]*)"$/ do |message|
   expect(stdout.messages).to include(message)
 end
 
+Given(/^the secret code is "([^"]*)"$/) do |arg1|
+  pending
+end
+
+When(/^I guess "([^"]*)"$/) do |arg1|
+  pending
+end
+
+Then(/^the mark should be "([^"]*)"$/) do |arg1|
+  pending
+end
+
