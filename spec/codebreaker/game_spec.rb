e608f7c817bfd4f20364e8f20d1fa32a78ad9db8
Jake Romer <jkrmr@users.noreply.github.com>
2015-07-07 00:42:54 -0400

    Refactor #guess

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -21,54 +21,13 @@ module Codebreaker
     end
 
     describe '#guess' do
-      context 'with no matches' do
-        it "sends a mark with ''" do
-          game.start('1234')
+      it 'sends the mark to output' do
+        game.start('1234')
+        allow(output).to receive(:puts).with('++++')
 
-          expect(output).to receive(:puts).with('')
+        game.guess('1234')
 
-          game.guess('5555')
-        end
-      end
-
-      context 'with 1 number match' do
-        it "sends a mark with '-'" do
-          game.start('1234')
-
-          expect(output).to receive(:puts).with('-')
-
-          game.guess('2555')
-        end
-      end
-
-      context 'with 1 exact match' do
-        it "sends a mark with '+'" do
-          game.start('1234')
-
-          expect(output).to receive(:puts).with('+')
-
-          game.guess('1555')
-        end
-      end
-
-      context 'with 2 number matches' do
-        it "sends a mark with '--'" do
-          game.start('1234')
-
-          expect(output).to receive(:puts).with('--')
-
-          game.guess('2355')
-        end
-      end
-
-      context 'with 1 number match and 1 exact match (in that order)' do
-        it "sends a mark with '+-'" do
-          game.start('1234')
-
-          expect(output).to receive(:puts).with('+-')
-
-          game.guess('2535')
-        end
+        expect(output).to have_received(:puts).with('++++')
       end
     end
   end
--- spec/codebreaker/marker_spec.rb
+++ spec/codebreaker/marker_spec.rb
@@ -43,5 +43,47 @@ module Codebreaker
         end
       end
     end
+
+    describe '#number_match_count' do
+      context 'with no matches' do
+        it 'returns 0' do
+          marker = Marker.new(secret: '1234', guess: '5555')
+
+          matches = marker.number_match_count
+
+          expect(matches).to eq 0
+        end
+      end
+
+      context 'with 1 number match' do
+        it 'returns 1' do
+          marker = Marker.new(secret: '1234', guess: '2555')
+
+          matches = marker.number_match_count
+
+          expect(matches).to eq 1
+        end
+      end
+
+      context 'with 1 exact match' do
+        it 'returns 0' do
+          marker = Marker.new(secret: '1234', guess: '1555')
+
+          matches = marker.number_match_count
+
+          expect(matches).to eq 0
+        end
+      end
+
+      context 'with 1 exact match and 1 number match' do
+        it 'returns 1' do
+          marker = Marker.new(secret: '1234', guess: '1525')
+
+          matches = marker.number_match_count
+
+          expect(matches).to eq 1
+        end
+      end
+    end
   end
 end
