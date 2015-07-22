9433b7539761c977bcd4ceba5b12e03195ef37c9
Jake Romer <jkrmr@users.noreply.github.com>
2015-07-22 16:51:35 -0400

    Fix failing cuke test

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -1,18 +1,16 @@
 # Messenger double for standard out
 class Messenger
-
   def messenger
     @messenger ||= StringIO.new
   end
 
   def messages
-    messenger.string.split("\n")
+    messenger.string.split("\n") << ''
   end
 
   def puts(*args)
     messenger.puts(*args)
   end
-
 end
 
 def stdout
